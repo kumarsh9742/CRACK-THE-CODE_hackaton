4378ab5944b34ace79bd71fe6e323dbc862458cc
kumarsh9742 <66528958+kumarsh9742@users.noreply.github.com>
2020-07-25 13:38:13 +0530

    Add files via upload

---
+++ hacathon/script.js
@@ -0,0 +1,13 @@
+
+ var mainDiv = document.createElement("div");
+ mainDiv.setAttribute("class","outerDiv");
+ document.body.append(mainDiv);
+
+ var mainDivstyle = document.querySelector(".outerDiv");
+ mainDivstyle.style.width="80%";
+ mainDivstyle.style.height="1350px";
+ mainDivstyle.style.margin="0 auto";
+ mainDivstyle.style.backgroundColor="cyan";
+ mainDivstyle.style.postion="relative";
+ mainDivstyle.style.top="40%";
+
